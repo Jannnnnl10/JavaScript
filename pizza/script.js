88c52092579e87383fb2cb360bea2ce42bebfc4f
Jan Lehmann <janp.lehmann@icloud.com>
2024-11-15 09:12:25 +0100

    Pizza bestellen Aufgabe abgeschlossen

--- pizza/script.js
+++ pizza/script.js
@@ -1,23 +1,24 @@
 window.addEventListener("load", initialize);
+var ingredients;
+var choice; 
 
 function initialize() {
     document.getElementById("add").addEventListener("click", add);
     document.getElementById("remove").addEventListener("click", remove);
+
+    ingredients = document.getElementById("ingredients");
+    choice = document.getElementById("choice"); // Nimmt <select> für den rechten Balken
 }
 
 function add() {
-    var ingredients = document.getElementById("ingredients");
-    var choice = document.getElementById("choice");
-
-    if (ingredients.selectedIndex > -1) {
-        var selectedOption = ingredients.options[ingredients.selectedIndex];
-        choice.appendChild(selectedOption);
+     
+    if (ingredients.selectedIndex > -1) { // Schaut ob in die linke Liste geklickt wurde
+        var selectedOption = ingredients.options[ingredients.selectedIndex]; // Wählt das Ausgewählte Item
+        choice.appendChild(selectedOption); // Schiebt das Element von Links nach rechts
     }
 }
 
 function remove() {
-    var choice = document.getElementById("choice");
-    var ingredients = document.getElementById("ingredients");
 
     if (choice.selectedIndex > -1) {
         var selectedOption = choice.options[choice.selectedIndex];
